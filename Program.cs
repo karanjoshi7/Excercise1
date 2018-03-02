25927aec70bd2830bb877c5f2d3caf05535e32f4
Karan Joshi <10010119@student.toiohomai.ac.nz>
2018-03-02 17:49:46 +1300

    first commit

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+           Console.WriteLine("!!!!!!!    Hello   !!");                               // Write a program to print 'Hello' on screen and then print your name on a separate line.//
+           Console.WriteLine("  !!!!!! Karan Joshi !!");
+            Console.ReadKey();
+
+
+            
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+            /*int firstnumber= 74;
+              int secondnumber = 36;                                                   //Write a program to print the sum of two numbers//
+              int output;
+              output = firstnumber + secondnumber;
+              Console.WriteLine(" Output is {0}", output);
+               Console.ReadKey();*/
+
+
+            
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+            /*int firstnumber = 50;
+            int secondnumber = 3;                                                     // Write a program to divide two numbers and print on the screen.//
+            int output;
+            output = firstnumber / secondnumber;
+            Console.WriteLine(" Output is {0}", output);
+             Console.ReadKey();*/
+
+            
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+            
+
+            /* int firstnumber= 25;
+            int secondnumber = 5;                                                     //Write a program that takes two numbers as input and display the product of two numbers.//
+            int output;
+            output = firstnumber * secondnumber;
+            Console.WriteLine(" Output is {0}", output);
+             Console.ReadKey();*/
+
+            
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+           
+             
+            /*int j,n;
+	         Console.WriteLine("\n\n");
+             Console.WriteLine("Display the multiplication table:\n");                 //Write a program that takes a number as input and prints its multiplication table upto 10//
+             Console.WriteLine("************************************");
+             Console.WriteLine("\n\n");   
+
+             Console.WriteLine("Input the number (Table to be calculated) : ");
+             n= Convert.ToInt32(Console.ReadLine());   
+             Console.WriteLine("\n");
+            for(j=1;j<=10;j++)
+            {
+             Console.WriteLine("{0} X {1} = {2} \n",n,j,n*j);
+             Console.ReadLine();
+                
+            }*/
+
+            
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+           
+
+              /*double r,perimeter, area;
+              Console.Write("Please write the radius of your circle => ");
+              r = Convert.ToDouble(Console.ReadLine());                              //Write a program to print the area and perimeter of a circle.//
+              perimeter = 2 * 3.14 * r;
+              area = 3.14 * Math.Pow(r, 2); 
+              Console.WriteLine("********************************************"); 
+              Console.WriteLine("The perimeter of yor circle : {0}",perimeter);
+              Console.WriteLine("The area of yor circle : {0}", area);
+              Console.ReadKey();*/
+
+            
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+            
+              /*int num;
+              Console.Write("\n\n");
+              Console.Write("Check whether a number is positive or negative:\n");                     //Write a program to get a number from the user and print whether it is positive or negative.//
+              Console.Write("*************************************************");
+              Console.Write("\n\n");
+              Console.Write("Input an integer : ");
+              num= Convert.ToInt32(Console.ReadLine());
+             if (num >= 0)
+ 
+             Console.WriteLine("{0} is a positive number.\n",num);
+             else
+             Console.WriteLine("{0} is a negative number. \n", num);
+             Console.ReadKey();*/
+
+            
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+       
+
+             /*int num1, num2, num3;
+             Console.Write("\n\n");
+             Console.Write("Find the largest of three numbers:\n");
+             Console.Write("------------------------------------");
+             Console.Write("\n\n");
+
+             Console.Write("Input the 1st number :");
+             num1 = Convert.ToInt32(Console.ReadLine());
+             Console.Write("Input the  2nd number :");
+             num2 = Convert.ToInt32(Console.ReadLine());
+             Console.Write("Input the 3rd  number :");
+             num3 = Convert.ToInt32(Console.ReadLine());
+ 
+             if (num1 > num2)
+          { 
+             if (num1 > num3)
+           {
+             Console.Write("The 1st Number is the greatest among three. \n\n");                //Take three numbers from the user and print the greatest number.//
+        }
+             else
+        {
+             Console.Write("The 3rd Number is the greatest among three. \n\n");
+        }
+    }
+            else if (num2 > num3)
+            Console.Write("The 2nd Number is the greatest among three \n\n");
+            else
+            Console.Write("The 3rd Number is the greatest among three \n\n");
+            Console.ReadKey();*/
+
+            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- // //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+        }
+    }
+}
+
+
